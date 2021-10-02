5409ff99d05dc7a52bf01fe8a330184db1339433
Clem-Tee <clementogol@gmail.com>
2021-10-02 15:23:58 +0300

    Added a header file

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef __MAINH__
+#define __MAINH__
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * struct print - printf functionparameters
+ * @a: conversion specifier to match
+ * @f: function to return
+ *
+ */
+typedef struct print
+{
+	char *a;
+	int (*f)(va_list);
+} _types;
+
+int putPercent(char s, char n);
+int printInt(va_list i);
+int printstring(va_list s);
+int printchar(va_list c);
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
